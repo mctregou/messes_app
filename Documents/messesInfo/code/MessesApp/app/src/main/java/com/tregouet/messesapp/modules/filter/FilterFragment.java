f36fbe41f8eba68ac87e674a06d6209e4c41089e
Marie-Cécile Trégouët <mc.tregouet@telecom-bretagne.eu>
2017-06-18 22:56:19 +0200

    [Search] Add search button

--- Documents/messesInfo/code/MessesApp/app/src/main/java/com/tregouet/messesapp/modules/filter/FilterFragment.java
+++ Documents/messesInfo/code/MessesApp/app/src/main/java/com/tregouet/messesapp/modules/filter/FilterFragment.java
@@ -1,9 +1,8 @@
-package com.tregouet.messesapp.modules.tuto;
+package com.tregouet.messesapp.modules.filter;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,18 +11,16 @@ import android.view.ViewGroup;
 import com.tregouet.messesapp.R;
 import com.tregouet.messesapp.modules.main.MainActivity;
 
-import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
-import de.hdodenhof.circleimageview.CircleImageView;
 
-public class TutoFragment extends Fragment {
+public class FilterFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        View view = inflater.inflate(R.layout.fragment_tuto, container, false);
+        View view = inflater.inflate(R.layout.fragment_filter, container, false);
 
         ButterKnife.bind(this, view);
 
@@ -31,8 +28,8 @@ public class TutoFragment extends Fragment {
     }
 
     @OnClick(R.id.back)
-    public void closeTuto(){
-        Log.i(getClass().getSimpleName(), "closeTuto()");
-        ((MainActivity)getActivity()).openTuto();
+    public void closeFilter(){
+        Log.i(getClass().getSimpleName(), "closeFilter()");
+        ((MainActivity)getActivity()).openFilter();
     }
 }
--- Documents/messesInfo/code/MessesApp/app/src/main/java/com/tregouet/messesapp/modules/main/MainActivity.java
+++ Documents/messesInfo/code/MessesApp/app/src/main/java/com/tregouet/messesapp/modules/main/MainActivity.java
@@ -1,13 +1,17 @@
 package com.tregouet.messesapp.modules.main;
 
+import android.media.Image;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ButtonBarLayout;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.tregouet.messesapp.R;
+import com.tregouet.messesapp.modules.filter.FilterFragment;
 import com.tregouet.messesapp.modules.search.SearchFragment;
 import com.tregouet.messesapp.modules.tuto.TutoFragment;
 
@@ -21,7 +25,12 @@ public class MainActivity extends AppCompatActivity {
     @BindView(R.id.logo)
     CircleImageView logo;
 
+    @BindView(R.id.search)
+    ImageView search;
+
     public boolean isTutoOpen = false;
+    public boolean isFilterOpen = false;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +49,7 @@ public class MainActivity extends AppCompatActivity {
     public void openTuto(){
         Log.i(getClass().getSimpleName(), "openTuto()");
         if (!isTutoOpen) {
+            search.setVisibility(View.GONE);
             FragmentManager fm = getSupportFragmentManager();
             FragmentTransaction fragmentTransaction = fm.beginTransaction();
             fragmentTransaction.add(R.id.frame, new TutoFragment());
@@ -50,6 +60,38 @@ public class MainActivity extends AppCompatActivity {
         } else {
             onBackPressed();
             isTutoOpen = false;
+            search.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @OnClick(R.id.search)
+    public void openFilter(){
+        Log.i(getClass().getSimpleName(), "openTuto()");
+        if (!isFilterOpen) {
+            search.setVisibility(View.GONE);
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fm.beginTransaction();
+            fragmentTransaction.add(R.id.frame, new FilterFragment());
+            fragmentTransaction.addToBackStack("filter");
+            fragmentTransaction.setCustomAnimations(R.anim.slide_in_up, R.anim.slide_out_up);
+            fragmentTransaction.commit();
+            isFilterOpen = true;
+        } else {
+            onBackPressed();
+            isFilterOpen = false;
+            search.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        if (isTutoOpen){
+            search.setVisibility(View.VISIBLE);
+            isTutoOpen = false;
+        } else if (isFilterOpen){
+            search.setVisibility(View.VISIBLE);
+            isFilterOpen = false;
         }
     }
 }
